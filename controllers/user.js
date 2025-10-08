6cdd8a07725c2791249548066bcaeb3823958936
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-08 15:35:31 +1300

    Create user function

---
+++ controllers/user.js
@@ -0,0 +1,27 @@
+import prisma from "../prisma/client";
+
+const createUser = async (req, res) => {
+    try {
+        await prisma.user.create({
+            data: { 
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                phoneNumber: req.body.phoneNumber,
+                dob: req.body.dob,
+                email: req.body.email,
+                role: req.body.role,
+            },
+        });
+
+        const newUser = await prisma.user.findMany();
+
+        return res.status(201).json({
+            message: "User created successfully",
+            data: newUser,
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message,
+        });
+    }
+};
