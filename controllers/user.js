9afa75d52c3181b7932cf781494a548463fcf9ef
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-08 16:31:33 +1300

    Created getUsers and getUserID functions

--- controllers/user.js
+++ controllers/user.js
@@ -1,30 +1,62 @@
 import prisma from "../prisma/client.js";
 
 const createUser = async (req, res) => {
+  try {
+    await prisma.user.create({
+      data: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phoneNumber: req.body.phoneNumber,
+        dob: req.body.dob,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role,
+      },
+    });
+
+    const newUser = await prisma.user.findMany();
+
+    return res.status(201).json({
+      message: "User created successfully",
+      data: newUser,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+
+    if (!users) {
+      return res.status(404).json({ message: "No users found" });
+    }
+
+    return res.status(200).json({ data: users });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const getUserID = async (req, res) => {
     try {
-        await prisma.user.create({
-            data: { 
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                phoneNumber: req.body.phoneNumber,
-                dob: req.body.dob,
-                email: req.body.email,
-                password: req.body.password,
-                role: req.body.role,
-            },
-        });
+        const user = await prisma.user.findUnique();
 
-        const newUser = await prisma.user.findMany();
+        if(!user) {
+            return res.status(404).json({ message: "No user found" });
+        }
 
-        return res.status(201).json({
-            message: "User created successfully",
-            data: newUser,
-        });
-    } catch (err) {
+        return res.status(200).json({ data: users });
+    } catch(err) {
         return res.status(500).json({
             message: err.message,
         });
     }
 };
 
-export { createUser };
+export { createUser, getUsers, getUserID };
