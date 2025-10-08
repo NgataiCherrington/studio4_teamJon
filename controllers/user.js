1e3e87c2f528f0d3a7dd79f66beea06708251c74
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-08 16:49:59 +1300

    Added a where clause to updateUser function for it to work

--- controllers/user.js
+++ controllers/user.js
@@ -45,13 +45,15 @@ const getUsers = async (req, res) => {
 
 const getUserID = async (req, res) => {
     try {
-        const user = await prisma.user.findUnique();
+        const user = await prisma.user.findUnique({
+            where: { id: req.params.id },
+          });
 
         if(!user) {
             return res.status(404).json({ message: "No user found" });
         }
 
-        return res.status(200).json({ data: users });
+        return res.status(200).json({ data: user });
     } catch(err) {
         return res.status(500).json({
             message: err.message,
@@ -59,4 +61,64 @@ const getUserID = async (req, res) => {
     }
 };
 
-export { createUser, getUsers, getUserID };
+const updateUser = async (req, res) => {
+    try {
+        let user = await prisma.user.findUnique({
+            where: { id: req.params.id },
+        });
+
+        if(!user) {
+            return res.status(404).json({
+                message: `No User with id: ${req.params.id} found`,
+            });
+        }
+
+        user = await prisma.user.update({
+            where: { id: req.params.id },
+            data: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                phoneNumber: req.body.phoneNumber,
+                dob: req.body.dob,
+                email: req.body.email,
+            },
+        });
+
+        return res.status(200).json({
+            message: `User with id: ${req.params.id} successfully updated`,
+            data: user,
+        });
+    } catch(err) {
+        return res.status(500).json({
+            message: err.message,
+        });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: req.params.id },
+      });
+  
+      if (!user) {
+        return res.status(404).json({
+          message: `No User with the id: ${req.params.id} found`,
+        });
+      }
+  
+      await prisma.user.delete({
+        where: { id: req.params.id },
+      });
+  
+      return res.status(200).json({
+        message: `User with the id: ${req.params.id} successfully deleted`,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: err.message,
+      });
+    }
+  };
+
+export { createUser, getUsers, getUserID, updateUser, deleteUser };
