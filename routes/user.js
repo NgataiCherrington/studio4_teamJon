cc1d62077d2aafb88addb3862ff70bb267ed0c60
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-08 16:50:21 +1300

    Added update and delete user functions to route

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ import {
     createUser,
     getUsers,
     getUserID,
+    updateUser,
+    deleteUser
 } from "../controllers/user.js";
 
 const router = express.Router();
@@ -11,6 +13,7 @@ const router = express.Router();
 router.post("/", createUser);
 router.get("/", getUsers);
 router.get("/:id", getUserID);
-
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
