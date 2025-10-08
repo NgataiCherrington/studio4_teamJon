a03863b29a9c50970f4b357548070bf7607fe1d5
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-08 16:31:20 +1300

    Added line to parse raw JSON into a JS object

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 // Use the PORT environment variable or 3000
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api/users", userRoutes);
 app.use("/", indexRoutes);
 app.use(isContentTypeApplicationJSON);
