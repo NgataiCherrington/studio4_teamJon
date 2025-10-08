47dc3ec37dc359c7b417bbf30b83e11c3ad76abb
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-08 16:31:44 +1300

    Added newly created functions to route

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,15 @@ import express from "express";
 
 import {
     createUser,
+    getUsers,
+    getUserID,
 } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/:id", getUserID);
+
 
 export default router;
