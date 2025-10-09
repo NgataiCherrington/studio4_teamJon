332b216a41d90b868d5adeeb19aa39eec248f11c
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-10 09:58:18 +1300

    Fixing extension

--- controllers/wellness.js
+++ controllers/wellness.js
@@ -1,4 +1,4 @@
-import prisma from "../prisma/client";
+import prisma from "../prisma/client.js";
 
 const createWellness = async (req, res) => {
     try {
@@ -43,3 +43,86 @@ const getWellness = async (req, res) => {
         });
     }
 };
+
+const getWellnessID = async (req, res) => {
+    try {
+        const wellness = await prisma.wellness.findUnique({
+            where: { id: req.params.id },
+        });
+
+        if(!wellness) {
+            return res.status(404).json({
+                message: `No wellness data with id: ${req.params.id} found`,
+            });
+        }
+
+        return res.status(200).json({
+            data: wellness,
+        });
+    } catch(err) {
+        return res.status(500).json({
+            message: err.message,
+        });
+    }
+};
+
+const updateWellness = async (req, res) => {
+    try {
+        let wellness = await prisma.wellness.findUnique({
+            where: { id: req.params.id },
+        });
+
+        if(!wellness) {
+            return res.status(404).json({
+                message: `No wellness data with id: ${req.params.id} found`,
+            });
+        }
+
+        wellness = prisma.wellness.update({
+            where: { id: req.params.id },
+            data: {
+                sleep: req.body.sleep,
+                stress: req.body.stress,
+                fatigue: req.body.fatigue,
+                muscleSoreness: req.body.muscleSoreness,
+            }
+        });
+
+        return res.status(200).json({
+            message: `Wellness data with id: ${req.params.id} found`,
+            data: wellness,
+        });
+    } catch(err) {
+        return res.status(500).json({
+            message: err.message,
+        });
+    }
+};
+
+const deleteWellness = async (req, res) => {
+    try {
+        const wellness = await prisma.wellness.findUnique({
+            where: { id: req.params.id },
+        });
+
+        if(!wellness) {
+            return res.status(404).json({
+                message: `Wellness data with id: ${req.params.id} not found`,
+            });
+        }
+
+        await prisma.wellness.delete({
+            where: { id: req.params.id },
+        })
+
+        return res.status(200).json({
+            message: `Wellness data with id: ${req.params.id} found`,
+        });
+    } catch(err) {
+        return res.status(500).json({
+            message: err.message,
+        });
+    }
+};
+
+export { createWellness, getWellness, getWellnessID, updateWellness, deleteWellness };
