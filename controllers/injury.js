71b57f3e0e3f88f2eebc52d6b0ec925b9af4342b
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-10 11:48:07 +1300

    Wrapped injury info in data object

--- controllers/injury.js
+++ controllers/injury.js
@@ -1,123 +1,126 @@
 import prisma from "../prisma/client.js";
 
 const createInjury = async (req, res) => {
-    try {
-        const createInjury = await prisma.injury.create({
-            injuryCode: req.body.injuryCode,
-            timeOfInjury: req.body.timeOfInjury,
-            description: req.body.description
-        });
+  try {
+    await prisma.injury.create({
+      data: {
+        injuryCode: req.body.injuryCode,
+        timeOfInjury: req.body.timeOfInjury,
+        description: req.body.description,
+        userId: req.user.id,
+      },
+    });
 
-        const newInjury = await prisma.injury.findMany();
+    const newInjury = await prisma.injury.findMany();
 
-        return res.status(200).json({
-            message: "Injury data created successfully",
-            data: newInjury,
-        });
-    } catch(err){
-        return res.status(500).json({
-            message: err.message,
-        });
-    }
+    return res.status(201).json({
+      message: "Injury data created successfully",
+      data: newInjury,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
 };
 
 const getInjuries = async (req, res) => {
-    try {
-        const injuries = await prisma.injury.findMany();
+  try {
+    const injuries = await prisma.injury.findMany();
 
-        if(!injuries) {
-            return res.status(404).json({
-                message: "No teams found",
-            });
-        }
-
-        return res.status(200).json({
-            data: injuries,
-        });
-    } catch(err) {
-        return res.status(500).json({
-            message: err.message,
-        });
+    if (!injuries) {
+      return res.status(404).json({
+        message: "No teams found",
+      });
     }
+
+    return res.status(200).json({
+      data: injuries,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
 };
 
 const getInjuryID = async (req, res) => {
-    try {
-        const injury = await prisma.injury.findUnique({
-            where: { id: req.params.id },
-        });
+  try {
+    const injury = await prisma.injury.findUnique({
+      where: { id: req.params.id },
+    });
 
-        if(!injury) {
-            return res.status(404).json({
-                message: `No injury with id: ${req.params.id } found`,
-            });
-        }
-
-        return res.status(200).json({
-            data: injury,
-        });
-    } catch(err) {
-        return res.status(500).json({
-            message: err.message,
-        });
+    if (!injury) {
+      return res.status(404).json({
+        message: `No injury with id: ${req.params.id} found`,
+      });
     }
+
+    return res.status(200).json({
+      data: injury,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
 };
 
 const updateInjury = async (req, res) => {
-    try {
-        let injury = await prisma.injury.findUnique({
-            where: { id: req.params.id },
-        });
+  try {
+    let injury = await prisma.injury.findUnique({
+      where: { id: req.params.id },
+    });
 
-        if(!injury) {
-            return res.status(404).json({
-                message: `No injury with id: ${req.params.id} found`
-            });
-        }
-
-        await prisma.injury.update({
-            where: { id: req.params.id},
-            data: {
-                injuryCode: req.body.injuryCode,
-                timeOfInjury: req.body.timeOfInjury,
-                description: req.body.description,
-            },
-        });
-
-        return res.status(200).json({
-            message: `Injury data with id: ${req.params.id} successfully updated`,
-        });
-    } catch(err) {
-        return res.status(500).json({
-            message: err.message,
-        });
+    if (!injury) {
+      return res.status(404).json({
+        message: `No injury with id: ${req.params.id} found`,
+      });
     }
+
+    await prisma.injury.update({
+      where: { id: req.params.id },
+      data: {
+        injuryCode: req.body.injuryCode,
+        timeOfInjury: req.body.timeOfInjury,
+        description: req.body.description,
+      },
+    });
+
+    return res.status(200).json({
+      message: `Injury data with id: ${req.params.id} successfully updated`,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
 };
 
 const deleteInjury = async (req, res) => {
-    try {
-        const injury = await prisma.injury.findUnique({
-            where: { id: req.params.id },
-        });
+  try {
+    const injury = await prisma.injury.findUnique({
+      where: { id: req.params.id },
+    });
 
-        if(!injury) {
-            return res.status(404).json({
-                message: `No injury with id: ${req.params.id} found`,
-            });
-        }
-
-        await prisma.injury.delete({
-            where: { id: req.params.id },
-        })
-
-        return res.status(200).json({
-            message: `Injury with id: ${req.params.id } successfully deleted`,
-        });
-    } catch(err) {
-        return res.status(500).json({
-            message: err.message,
-        });
+    if (!injury) {
+      return res.status(404).json({
+        message: `No injury with id: ${req.params.id} found`,
+      });
     }
+
+    await prisma.injury.delete({
+      where: { id: req.params.id },
+    });
+
+    return res.status(200).json({
+      message: `Injury with id: ${req.params.id} successfully deleted`,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
 };
 
 export { createInjury, getInjuries, getInjuryID, updateInjury, deleteInjury };
