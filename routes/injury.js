0421389218be44c26f8c0e5738396657f06a3ab3
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-10 15:02:34 +1300

    Imported validation functions

--- routes/injury.js
+++ routes/injury.js
@@ -8,14 +8,19 @@ import {
     deleteInjury,
 } from "../controllers/injury.js";
 
+import {
+    validatePostInjury,
+    validatePutInjury,
+} from "../middleware/validation/injury.js";
+
 const router = express.Router();
 
 import jwtAuth from "../middleware/jwtauth.js";
 
-router.post("/", jwtAuth, createInjury);
+router.post("/", validatePostInjury, jwtAuth, createInjury);
 router.get("/", getInjuries);
 router.get("/:id", getInjuryID);
-router.put("/:id", updateInjury);
+router.put("/:id", validatePutInjury, updateInjury);
 router.delete("/:id", deleteInjury);
 
 export default router;
