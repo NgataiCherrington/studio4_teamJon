b0409658e1e87226c3900c098a9766f2db15bca3
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-10 15:02:43 +1300

    Imported validation functions

--- routes/wellness.js
+++ routes/wellness.js
@@ -8,14 +8,19 @@ import {
     deleteWellness,
 } from "../controllers/wellness.js";
 
+import {
+    validatePostWellness,
+    validatePutWellness,
+} from "../middleware/validation/wellness.js";
+
 const router = express.Router();
 
 import jwtAuth from "../middleware/jwtauth.js";
 
-router.post("/", jwtAuth, createWellness);
+router.post("/", validatePostWellness, jwtAuth, createWellness);
 router.get("/", getWellness);
 router.get("/:id", getWellnessID);
-router.put("/:id", updateWellness);
+router.put("/:id", validatePutWellness, updateWellness);
 router.delete("/:id", deleteWellness);
 
 export default router;
