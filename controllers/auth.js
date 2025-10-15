a95411737343fe308ea4c031a3c5aab2ab5d9529
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-15 18:15:40 +1300

    Created logout function

--- controllers/auth.js
+++ controllers/auth.js
@@ -106,4 +106,41 @@ const login = async (req, res) => {
   }
 };
 
-export { register, login };
+const logout = async (req, res) => {
+  try {
+    // Get token from request header
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+      return res.status(400).json({
+        message: "No token provided",
+      });
+    }
+
+    const token = authHeader.split(" ")[1]; // Remove Bearer
+
+    // Check if token is already blacklisted
+    const exists = await prisma.blacklist.findUnique({
+      where: { token },
+    });
+
+    if(exists) {
+      return res.status(200).json({
+        message: "Token already blacklisted",
+      });
+    }
+
+    await prisma.blacklist.create({
+      data: { token },
+    });
+
+    return res.status(200).json({
+      message: "User logged out successfully",
+    });
+  } catch(err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+}
+
+export { register, login, logout };
