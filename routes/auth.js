51a4fae7925e631cade8a3baac30ab4af3bac5b4
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-15 18:17:49 +1300

    Add logout route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { register, login } from "../controllers/auth.js";
+import { register, login, logout } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").post(logout);
 
 export default router;
