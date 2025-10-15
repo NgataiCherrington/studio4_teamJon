8ffceb965129733320ee9313a3524b42bfbe4d4b
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-15 15:35:51 +1300

    Filtering and pagination for wellness done

--- controllers/wellness.js
+++ controllers/wellness.js
@@ -18,7 +18,47 @@ const createWellness = async (req, res) => {
 
 const getWellness = async (req, res) => {
     try {
-        const wellness = await wellnessRepository.findAll();
+        // Deconstruct query parameters with default values for filtering and pagination
+    const {
+        sleep,
+        stress,
+        fatigue,
+        muscleSoreness,
+        sortBy = "id",
+        sortOrder = "asc",
+        page = 1,
+        pageSize = 10,
+      } = req.query;
+  
+      // Build a filters object based on query parameters
+      const filters = {};
+      if (sleep) filters.sleep = sleep;
+      if (stress) filters.stress = stress;
+      if (fatigue) filters.fatigue = fatigue;
+      if (muscleSoreness) filters.muscleSoreness = muscleSoreness;
+
+
+  
+      // Validate and normalize sort order, Default to 'asc' if invalid
+      const validSortOrders = ["asc", "desc"];
+      const order = validSortOrders.includes(sortOrder.toLowerCase())
+        ? sortOrder.toLowerCase()
+        : "asc";
+  
+      // Validate and normalize sort field, Default to 'id' if invalid
+      const validSortFields = ["id", "sleep", "stress", "fatigue", "muscleSoreness"];
+      const fields = validSortFields.includes(sortBy.toLowerCase())
+        ? sortBy.toLowerCase()
+        : "id";
+  
+      const wellness = await wellnessRepository.findAll(
+        filters,
+        fields,
+        order,
+        page,
+        pageSize
+      );
+        // const wellness = await wellnessRepository.findAll();
         if(!wellness) {
             return res.status(404).json({
                 message: "No wellness data found",
