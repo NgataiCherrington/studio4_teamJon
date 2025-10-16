ad6dacb19201a5236f7475828b30a1ba53fa68fd
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-16 15:33:08 +1300

    rbac control for delete in wellness route

--- routes/wellness.js
+++ routes/wellness.js
@@ -1,26 +1,33 @@
 import express from "express";
 
 import {
-    createWellness,
-    getWellness,
-    getWellnessID,
-    updateWellness,
-    deleteWellness,
+  createWellness,
+  getWellness,
+  getWellnessID,
+  updateWellness,
+  deleteWellness,
 } from "../controllers/wellness.js";
 
 import {
-    validatePostWellness,
-    validatePutWellness,
+  validatePostWellness,
+  validatePutWellness,
 } from "../middleware/validation/wellness.js";
+
+import jwtAuth from "../middleware/jwtauth.js";
+import rbac from "../middleware/rbac.js";
 
 const router = express.Router();
 
-import jwtAuth from "../middleware/jwtauth.js";
+router.post(
+    "/", 
+    validatePostWellness, 
+    jwtAuth, 
+    createWellness
+);
 
-router.post("/", validatePostWellness, jwtAuth, createWellness);
 router.get("/", getWellness);
 router.get("/:id", getWellnessID);
 router.put("/:id", validatePutWellness, updateWellness);
-router.delete("/:id", deleteWellness);
+router.delete("/:id", deleteWellness, rbac("ADMIN"));
 
 export default router;
