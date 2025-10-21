4ea00cd5fce7321740e5535d0ae51f68b94ed797
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-21 14:43:37 +1300

    Users can only update and delete their own data

--- routes/user.js
+++ routes/user.js
@@ -14,12 +14,13 @@ import {
 } from "../middleware/validation/user.js"
 
 const router = express.Router();
+import jwtAuth from "../middleware/jwtauth.js";
 import rbac from "../middleware/rbac.js";
 
 router.post("/", validatePostUser, createUser);
 router.get("/", getUsers);
 router.get("/:id", getUserID);
-router.put("/:id", validatePutUser, updateUser);
-router.delete("/:id", deleteUser, rbac("ADMIN"));
+router.put("/:id", jwtAuth,  rbac("ADMIN"), validatePutUser, updateUser);
+router.delete("/:id", jwtAuth,  rbac("ADMIN"), deleteUser);
 
 export default router;
