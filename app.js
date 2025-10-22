c41b017fe5aaf120c717fa7b1ab7f013c0793822
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-22 16:43:20 +1300

    Comment line

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(isContentTypeApplicationJSON);
+// app.use(cors({ origin: "http://localhost:5174" }))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
