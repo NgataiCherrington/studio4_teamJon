f1a896500f8e4cf7d000046fcfab5039038cf018
NgataiCherrington <chernj1@student.op.ac.nz>
2025-11-06 10:06:01 +1300

    imported prisma for use in route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { prisma } from "../prismaClient.js";
 
 import { register, login, logout } from "../controllers/auth.js";
 
 const router = express.Router();
+const prisma = new prisma();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
