c1dc434931095516455e91e3630a41b23f9ba57b
NgataiCherrington <chernj1@student.op.ac.nz>
2025-11-05 16:09:00 +1300

    removed requirement of all fields for update

--- middleware/validation/user.js
+++ middleware/validation/user.js
@@ -71,54 +71,47 @@ const validatePostUser = (req, res, next) => {
 
 const validatePutUser = (req, res, next) => {
     const userSchema = Joi.object({
-        firstName: Joi.string().min(3).max(100).required().messages({
+        firstName: Joi.string().min(3).max(100).optional().messages({
             "string.base": "firstName should be a string",
             "string.empty": "firstName cannot be empty",
             "string.min": "firstName should have a minimum length of {#limit}",
             "string.max": "firstName should have a maximum  length of {#limit}",
-            "any.required": "firstName is required"
         }),
-        lastName: Joi.string().min(3).max(100).required().messages({
+        lastName: Joi.string().min(3).max(100).optional().messages({
             "string.base": "lastName should be a string",
             "string.empty": "lastName cannot be empty",
             "string.min": "lastName should have a minimum length of {#limit}",
             "string.max": "lastName should have a maximum  length of {#limit}",
-            "any.required": "lastName is required"
         }),
-        phoneNumber: Joi.string().min(3).max(100).required().messages({
+        phoneNumber: Joi.string().min(3).max(100).optional().messages({
             "string.base": "phoneNumber should be a string",
             "string.empty": "phoneNumber cannot be empty",
             "string.min": "phoneNumber should have a minimum length of {#limit}",
             "string.max": "phoneNumber should have a maximum  length of {#limit}",
-            "any.required": "phoneNumber is required"
         }),
-        dob: Joi.string().min(3).max(100).required().messages({
+        dob: Joi.string().min(3).max(100).optional().messages({
             "string.base": "dob should be a string",
             "string.empty": "dob cannot be empty",
             "string.min": "dob should have a minimum length of {#limit}",
             "string.max": "dob should have a maximum  length of {#limit}",
-            "any.required": "dob is required"
         }),
-        email: Joi.string().min(3).max(100).required().messages({
+        email: Joi.string().min(3).max(100).optional().messages({
             "string.base": "email should be a string",
             "string.empty": "email cannot be empty",
             "string.min": "email should have a minimum length of {#limit}",
             "string.max": "email should have a maximum  length of {#limit}",
-            "any.required": "email is required"
         }),
-        password: Joi.string().min(3).max(100).required().messages({
+        password: Joi.string().min(3).max(100).optional().messages({
             "string.base": "password should be a string",
             "string.empty": "password cannot be empty",
             "string.min": "password should have a minimum length of {#limit}",
             "string.max": "password should have a maximum  length of {#limit}",
-            "any.required": "password is required"
         }),
-        role: Joi.string().min(3).max(100).required().messages({
+        role: Joi.string().min(3).max(100).optional().messages({
             "string.base": "role should be a string",
             "string.empty": "role cannot be empty",
             "string.min": "role should have a minimum length of {#limit}",
             "string.max": "role should have a maximum  length of {#limit}",
-            "any.required": "role is required"
         }),
     }).min(1);
 
