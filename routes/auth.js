75488420c2c32517b04d13bd8e3427010170084b
NgataiCherrington <chernj1@student.op.ac.nz>
2025-11-06 10:08:56 +1300

    corrected prisma import

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { prisma } from "../prismaClient.js";
+import prisma from "../prisma/client.js";
 
 import { register, login, logout } from "../controllers/auth.js";
 
 const router = express.Router();
-const prisma = new prisma();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
