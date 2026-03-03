f270a2ca5f183779ff3f747c91485ce63a154205
Ngatai Cherrington (1000116322) <CHERNJ1@student.op.ac.nz>
2026-03-03 14:35:13 +1300

    pushing everything

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,7 +19,7 @@ const register = async (req, res) => {
     });
 
     if (user) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({ message: "Email already in use. Enter Another" });
     }
 
     // Generate a random salt to make the password hash unique
---
+++ prisma/migrations/20251105231443_fix/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."Team" ALTER COLUMN "userId" DROP NOT NULL;
